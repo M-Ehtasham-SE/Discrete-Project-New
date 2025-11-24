ac84f93d2f7713e5d1b4185cdb0f0fc2359fc2ea
Hafiz Muhammad Faheem Akhtar <f233066@cfd.nu.edu.pk>
2025-11-24 23:24:23 +0500

    Create AllWidgets.cpp

---
+++ AllWidgets.cpp
@@ -0,0 +1,5 @@
+// Placeholder implementations for remaining widgets
+#include "AllWidgets.h"
+
+// Empty source files will be created by moc if needed
+// Each widget is implemented inline in the header for simplicity
